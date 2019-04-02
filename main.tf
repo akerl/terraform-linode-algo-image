a9c70099e77dc23d31211b927eb91c489098a808
Les Aker <me@lesaker.org>
2019-04-02 20:12:41 +0100

    update module

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "random_id" "vm_suffix" {
 
 module "vm" {
   source          = "armorfret/algo-base/linode"
-  version         = "0.0.4"
+  version         = "0.0.5"
   name            = "algo-image_${random_id.vm_suffix.hex}"
   region          = "us-east"
   type            = "g6-standard-1"
